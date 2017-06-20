8e0e221219fa7e38f4101d86891d4b27ec270625
Vasileios Ioannidis <ioann006@ad.umn.edu>
2017-06-19 20:39:20 -0500

    earth

--- libGF/datasets/readEarthquakeDataset.m
+++ libGF/datasets/readEarthquakeDataset.m
@@ -6,7 +6,11 @@ function [m_spatialAdjacency,m_economicSectorsSignals] = readEarthquakeDataset
 pth = which('readEarthquakeDataset');
 folder=[pth(1:end-(length('readEarthquakeDataset')+2))  'EarthquakeDataset\'];
 
-file=strcat(folder,'IOUse_Before_Redefinitions_PRO_1997-2015_Summary.xlsx');
+file=strcat(folder,'databaseR.xlsx');
+[num,txt,raw]=xlsread(file);
+m_aux=readtable(file);
+m_aux.Date=datetime(m_aux.Date,'InputFormat','MM/dd/yyyy');
+m_aux.Date=datenum(m_aux.Date);
 m_economicSectorsSignals=zeros(71,19);
 t_inputOutput=zeros(71,71,19);
 for s_year=1997:2015
@@ -37,3 +41,66 @@ m_economicSectorsSignals=m_economicSectorsSignals(:,2:end);
 
 end
 
+function s_value=f(d)
+s_value=vpa(exp(-d.^2));
+end
+
+function [d1km d2km]=lldistkm(latlon1,latlon2)
+% format: [d1km d2km]=lldistkm(latlon1,latlon2)
+% Distance:
+% d1km: distance in km based on Haversine formula
+% (Haversine: http://en.wikipedia.org/wiki/Haversine_formula)
+% d2km: distance in km based on Pythagoras’ theorem
+% (see: http://en.wikipedia.org/wiki/Pythagorean_theorem)
+% After:
+% http://www.movable-type.co.uk/scripts/latlong.html
+%
+% --Inputs:
+%   latlon1: latlon of origin point [lat lon]
+%   latlon2: latlon of destination point [lat lon]
+%
+% --Outputs:
+%   d1km: distance calculated by Haversine formula
+%   d2km: distance calculated based on Pythagoran theorem
+%
+% --Example 1, short distance:
+%   latlon1=[-43 172];
+%   latlon2=[-44  171];
+%   [d1km d2km]=distance(latlon1,latlon2)
+%   d1km =
+%           137.365669065197 (km)
+%   d2km =
+%           137.368179013869 (km)
+%   %d1km approximately equal to d2km
+%
+% --Example 2, longer distance:
+%   latlon1=[-43 172];
+%   latlon2=[20  -108];
+%   [d1km d2km]=distance(latlon1,latlon2)
+%   d1km =
+%           10734.8931427602 (km)
+%   d2km =
+%           31303.4535270825 (km)
+%   d1km is significantly different from d2km (d2km is not able to work
+%   for longer distances).
+%
+% First version: 15 Jan 2012
+% Updated: 17 June 2012
+%--------------------------------------------------------------------------
+
+radius=6371;
+lat1=latlon1(1)*pi/180;
+lat2=latlon2(1)*pi/180;
+lon1=latlon1(2)*pi/180;
+lon2=latlon2(2)*pi/180;
+deltaLat=lat2-lat1;
+deltaLon=lon2-lon1;
+a=sin((deltaLat)/2)^2 + cos(lat1)*cos(lat2) * sin(deltaLon/2)^2;
+c=2*atan2(sqrt(a),sqrt(1-a));
+d1km=radius*c;    %Haversine distance
+
+x=deltaLon*cos((lat1+lat2)/2);
+y=deltaLat;
+d2km=radius*sqrt(x*x + y*y); %Pythagoran distance
+
+end
